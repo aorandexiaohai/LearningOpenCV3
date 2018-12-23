21428db0c9de23fcf2c8f330469947c7408db659
wuzhenhai <you@example.com>
2018-12-23 04:41:45 -0800

    code

---
+++ Chapter2/build.sh
@@ -0,0 +1,1 @@
+g++ src_2_1.cpp -I /usr/local/include/opencv4/ -lopencv_calib3d -lopencv_core -lopencv_dnn -lopencv_features2d -lopencv_flann -lopencv_gapi -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_ml -lopencv_objdetect -lopencv_photo -lopencv_stitching -lopencv_videoio -lopencv_video  -Wl,-rpath,/usr/local/lib:'$ORIGIN'
---
+++ Chapter2/src_2_1.cpp
@@ -0,0 +1,22 @@
+//A simple OpenCV program that loads an image from disk and displays it on the screen
+#include <opencv4/opencv2/opencv.hpp>
+#include <iostream>
+int main(int argc, char** argv)
+{
+    if(argc<2)
+    {
+        std::cout << "参数缺少" << std::endl;
+        exit(-1);
+    }
+    cv::Mat img = cv::imread(argv[1], -1);
+    if(img.empty())
+    {
+        std::cout << "空图像" << std::endl;
+        exit(-1);
+    }
+    cv::namedWindow("Example1", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Example1", img);
+    cv::waitKey(0);
+    cv::destroyWindow("Example1");
+    return 0;
+}
